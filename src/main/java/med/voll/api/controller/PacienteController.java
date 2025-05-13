8d8da3dc925b4a7ffd855b753082606b0e1416b4
Tais <taiscoliveiratec@gmail.com>
2025-05-13 20:08:47 -0300

    Controller do Paciente cadastrando

---
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -0,0 +1,22 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.domain.paciente.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+        repository.save(new Paciente(dados));
+    }
+
+}
