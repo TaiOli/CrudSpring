ee7ae24dbe98bc6b91f7bb4da4b86dd693dd7a45
Tais <taiscoliveiratec@gmail.com>
2025-05-13 11:31:51 -0300

    Implementação do método detalhar médico

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -65,4 +65,10 @@ public class MedicoController {
         repository.excluirFisicamente(id);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+    }
 }
