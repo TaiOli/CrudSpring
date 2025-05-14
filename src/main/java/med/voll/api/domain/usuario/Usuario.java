3b0c9258cda32e7b0a96370d3e6f0fbe6a0f30d6
Tais <taiscoliveiratec@gmail.com>
2025-05-14 08:50:28 -0300

    Criação da interface repository responsável pelo acesso na tabela de usuários e a classe AutenticacaoService  que simboliza o serviço de autenticação

--- src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 
-public class usuario {
+public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
